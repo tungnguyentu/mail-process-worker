44cb02b542c182b380942a951124f37a693f0e3e
tungnguyentu <tungnguyentu@bizflycloud.vn>
2021-12-08 20:14:13 +0700

    added log tracking

--- mail_process_worker/logic/client/mqtt_client.py
+++ mail_process_worker/logic/client/mqtt_client.py
@@ -78,6 +78,8 @@ class MQTTClient:
             payload = msg.get("payload", {})
             qos = msg.get("qos", 1)
             mqtt_topic = msg.get("topic")
+            p = json.loads(payload)
+            logger.info(f"LOG TRACKING: {p.get('user')}-{p.get('mailbox')}-{p.get('uids')}")
             logger.info("SENDING MESSAGE: {} TO TOPIC: {}".format(payload, mqtt_topic))
             mqtt_publish.single(
                 topic=mqtt_topic,
