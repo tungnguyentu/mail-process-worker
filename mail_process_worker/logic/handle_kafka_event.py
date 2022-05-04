fab5107e8eaf658999ee5f6bdb0ca56e1d342796
tungnguyentu <tungnguyentu@bizflycloud.vn>
2022-05-04 10:17:53 +0700

    remove aggregate event move

--- mail_process_worker/logic/handle_kafka_event.py
+++ mail_process_worker/logic/handle_kafka_event.py
@@ -37,7 +37,7 @@ class HandleEvent:
             "MessageAppend": 4,
             "FlagsSet": 5,
             "FlagsClear": 5,
-            "MessageMove": 6,
+            "MessageExpunge": 6,
             "MessageTrash": 7,
             "MailboxDelete": 8,
         }
@@ -108,13 +108,6 @@ class HandleEvent:
             "from", ""
         ):
             return self.set_priority(data)
-        if data["event"] in ["MessageAppend", "MessageExpunge"]:
-            try:
-                self.custom_event(data)
-            except Exception as e:
-                logger.info("Create Message Move Error: {}".format(e))
-                raise e
-            return
         return self.set_priority(data)
 
     def delay_event(self, user, message_id_header):
